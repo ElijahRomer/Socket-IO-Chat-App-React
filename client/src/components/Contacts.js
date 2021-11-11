c1f111eb34baf360432aacb563909139cdaa838b
Elijah Romer <romere88@gmail.com>
2021-11-11 15:30:18 -0600

    set up contactsContext and finished the contacts component rendering of contacts from localStorage

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { ListGroup } from 'react-bootstrap';
+import { useContactsContext } from '../contexts/ContactsProvider';
 
 export default function Contacts() {
-  return <div>Contacts</div>;
+  const { contacts } = useContactsContext();
+
+  return (
+    <ListGroup variant="flush">
+      {contacts.map((contact) => {
+        return <ListGroup.Item key={contact.id}>{contact.name}</ListGroup.Item>;
+      })}
+    </ListGroup>
+  );
 }
--- client/src/components/NewContactModal.js
+++ client/src/components/NewContactModal.js
@@ -1,14 +1,17 @@
 import React, { useRef } from 'react';
 import { Modal, Form, Button } from 'react-bootstrap';
+import { useContactsContext } from '../contexts/ContactsProvider';
 
 export default function NewContactModal({ closeModal }) {
   const idRef = useRef();
   const nameRef = useRef();
+  // returns a context object (similar to state) and we destructure the createContact method out of that object.
+  const { createContact } = useContactsContext();
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    // createContact(idRef.current.value, nameRef.current.value);
+    createContact(idRef.current.value, nameRef.current.value);
     closeModal();
   }
 
